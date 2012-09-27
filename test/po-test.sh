90c6a903dcd6d253a1bae9ad0154ea406316667a
John Moore <jmoore@zedstar.org>
2012-09-27 15:00:00 +0100

    test script by Antonio Dariush Kheirkhahzadeh

---
+++ test/po-test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "PO test"
+init()
+{ 
+for file in ../examples/*
+do
+    if [ ${file: -4} == ".xml" ]
+    then
+	schema=${file: 0:-4}
+	pofile=${file: 0:-4}
+	newxml=${file: 0:-4}
+	encode() 
+	{
+	    for i in {1..1}
+	    do
+		./../src/packedobjects --schema $schema.xsd --in $file  --out $pofile.po
+	    done
+	}
+	decode()
+	{
+	    for i in {1..1}
+	    do
+		./../src/packedobjects --schema $schema.xsd --in $pofile.po  --out $newxml.new.xml
+	    done
+	}
+	differ()
+	{
+	    if diff --ignore-all-space --ignore-blank-lines -y --suppress-common-lines $file $newxml.new.xml; then
+		:
+	    else
+		echo -e "----------------------------------------------------------------------------------"
+	    fi
+	}
+
+	    encode
+	    decode
+	    differ
+            # difference=$(differ)
+	    xmlsize=$(wc -c $file | egrep "[0-9]{1,}" -o)
+	    posize=$(wc -c $pofile.po | egrep "[0-9]{1,}" -o)
+	    ratio=$(echo "scale=3; ${posize}/${xmlsize}" | bc)
+	    percentage=$(echo "scale=3;(${posize}/${xmlsize})*100" | bc)
+	    echo -e "File: ${xmlsize} / ${posize} CR ${ratio} - ${percentage}%   \t${file}"
+	    # echo "$difference"
+    fi
+done 
+} 
+init
+rm ../examples/*.po
+rm ../examples/*.new.xml
