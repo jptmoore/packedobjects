1e6c35cf1db788319a40406f549c5763d02b14cc
John Moore <jmoore@zedstar.org>
2013-04-11 13:38:23 +0100

    adding error codes and remove param to unused parameter to init func

--- src/packedobjects_init.c
+++ src/packedobjects_init.c
@@ -21,7 +21,7 @@
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 
-static packedobjectsContext *_init_packedobjects(const char *schema_file)
+static packedobjectsContext *_init_packedobjects()
 {
   packedobjectsContext *pc = NULL;
   
@@ -42,6 +42,7 @@ static packedobjectsContext *_init_packedobjects(const char *schema_file)
   pc->decodep = NULL;
   pc->pdu_size = 0;
   pc->bytes = 0;
+  pc->init_error = 0;
   pc->encode_error = 0;
   pc->decode_error = 0;
 
@@ -54,43 +55,43 @@ packedobjectsContext *init_packedobjects(const char *schema_file, size_t bytes)
   packedobjectsContext *pc = NULL;
 
   // do the real allocation of structure with the provided schema
-  if ((pc = _init_packedobjects(schema_file)) == NULL) {
-    return NULL;
+  if ((pc = _init_packedobjects()) == NULL) {
+    return INIT_FAILED;
   }
 
   // store the schema we will work with
   if (schema_setup_schema(pc, schema_file) == -1) {
-    return NULL;
+    return INIT_SCHEMA_SETUP_FAILED;
   }  
   
   // used to store the encoded data
   if (encode_make_memory(pc, bytes) == -1) {
-    return NULL;
+    return INIT_ENCODE_SETUP_FAILED;
   }
   
   // validate the schema conforms to PO schema
   if (schema_validate_schema(pc) == -1) {
-    return NULL;
+    return INIT_SCHEMA_VALIDATION_FAILED;
   }
   
   // initialise xml validation code for later 
   if (schema_setup_validation(pc) == -1) {
-    return NULL;
+    return INIT_SETUP_VALIDATION_FAILED;
   }
   
   // expand user defined types in schema
   if (expand_make_expanded_schema(pc) == -1) {
-    return NULL;
+    return INIT_EXPANDED_SCHEMA_FAILED;
   }
 
   // make the canonical schema used for encoding
   if (canon_make_canonical_schema(pc) == -1) {
-    return NULL;
+    return INIT_CANON_SCHEMA_FAILED;
   }
 
   // setup xpath to use during encode
   if (schema_setup_xpath(pc) == -1) {
-    return NULL;
+    return INIT_XPATH_SETUP_FAILED;
   }
   
   return pc;
